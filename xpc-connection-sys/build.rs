6acf8d8da449d51a262df639e29a0622af7dae7c
Steven Joruk <sjoruk@beyondtrust.com>
2021-06-07 21:27:30 +0100

    Support secure client validation by exposing the audit token (#32)
    * Support validating clients
    * Fix deprecated bindgen warnings (whitelist/allowlist)

--- xpc-connection-sys/build.rs
+++ xpc-connection-sys/build.rs
@@ -7,26 +7,12 @@ fn main() {
     let mut builder = bindgen::Builder::default()
         .header("wrapper.h")
         .rustfmt_bindings(true)
-        .whitelist_function("dispatch_queue_create")
-        .whitelist_function("xpc.*")
-        .whitelist_type("_os_alloc_once_s")
-        .whitelist_type("uuid_t")
-        .whitelist_type("xpc_global_data")
-        .whitelist_type("xpc_pipe_t")
-        .whitelist_var("XPC.*")
-        .whitelist_var("_os_alloc_once_table")
-        .whitelist_var("_xpc.*")
-        .whitelist_var("xpc.*");
-
-    if let Ok(cflags) = std::env::var("CFLAGS") {
-        let cflags = cflags
-            .split_ascii_whitespace()
-            .map(|slice| slice.to_string())
-            .collect::<Vec<String>>();
-        builder = builder.clang_args(cflags);
-    }
-
-    builder
+        .allowlist_function("dispatch_queue_create")
+        .allowlist_function("xpc.*")
+        .allowlist_var("xpc.*")
+        .allowlist_var("_xpc.*")
+        .allowlist_var("XPC.*")
+        .allowlist_type("uuid_t")
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file(out_path.join("bindings.rs"))
